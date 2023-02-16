b45e79b836ff9ef736ce4baf53ed4f3facb8d4d9
mohamed561 <mrajawi561@gmail.com>
2023-02-16 09:37:36 -0800

    hhhhhh omg thisi is long

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main funcion
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int C;
+	long long int d;
+	float f;
+
+	printf("Sizo of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
+	printf("Size of a int: %lu byte(S)\n", (unsigned long)sizeof(b));
+	prinf("Size of a long int: %lu byte(S)\n" (unsigned long)sizeof(C));
+	prinf("Size of a long long int: %lu byte(S)\n" (unsigned long)sizeof(d));
+	prinf("Size of a float: %lu byte(S)\n" (unsigned long)sizeof(f));
+}
