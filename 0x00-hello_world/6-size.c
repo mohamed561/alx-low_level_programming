f9871039cb93d6b49aeefb189e74235fb79c6e61
mohamed561 <mrajawi561@gmail.com>
2023-09-07 11:51:35 -0700

    auto commit using alias

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Description: Sixth Task (Size is not grandeur,
+ * and territory does not make a nation)
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+}
