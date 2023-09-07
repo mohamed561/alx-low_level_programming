70f4be1e33ca8658ae41f6c78e5364c4fdc776fc
mohamed561 <mrajawi561@gmail.com>
2023-09-07 11:10:16 -0700

    auto commit using alias

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Taskfour (Hello, puts)
+ *
+ * Description: this program prints exactly whats inside puts(...)
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
