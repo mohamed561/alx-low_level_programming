ec89bb5c6b524ca47fd5b1c9235a74e69fef374c
mohamed561 <mrajawi561@gmail.com>
2023-09-07 11:25:33 -0700

    auto commit using alias

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Taskfive (Hello, printf)
+ *
+ * Description: This function prints whats inside printf ()
+ *
+ * Return: Alawys 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
