920098acc451ab1bf44104b70d451df0749f4d1b
mohamed561 <mrajawi561@gmail.com>
2023-09-08 08:20:36 -0700

    0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - The main function
+ *
+ * Description: TASKzero - Positive anything is better than negative nothing
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("positive\n");
+}
+else if (n == 0)
+{
+	printf("zero\n");
+}
+else
+{
+	printf("negative\n");
+}
+	return (0);
+}
